4c0c33cdd2301db3f2f7a0edcc5667f90cd24efe
lizongliang <lzl_anonym@aliyun.com>
2019-08-26 20:44:04 +0800

    init

---
+++ src/main/java/com/anonym/AnonymApplication.java
@@ -0,0 +1,16 @@
+package com.anonym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizongliang
+ */
+@SpringBootApplication
+public class AnonymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymApplication.class, args);
+    }
+
+}
