50e9cd4010abae23218a8d932d2312bf0107b351
lizongliang <lizongliang@gangquan360.com>
2019-10-25 16:33:02 +0800

    2019-10-25 微信小程序

---
+++ src/main/java/com/anonym/module/wechat/miniprogram/domain/WeChatLoginResultDTO.java
@@ -0,0 +1,17 @@
+package com.anonym.module.wechat.miniprogram.domain;
+
+import lombok.Data;
+
+/**
+ * 微信 登录结果DTO类
+ */
+@Data
+public class WeChatLoginResultDTO {
+
+    private String openId;
+
+    private String sessionKey;
+
+    private String unionId;
+
+}
