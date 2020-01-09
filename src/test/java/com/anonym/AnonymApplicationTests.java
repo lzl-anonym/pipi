0356dc09961eb5a8345e65a153d8210b339b4311
lizongliang <lizongliang@gangquan360.com>
2020-01-09 15:08:01 +0800

    测试类

--- src/test/java/com/anonym/AnonymApplicationTests.java
+++ src/test/java/com/anonym/AnonymApplicationTests.java
@@ -1,16 +1,26 @@
 package com.anonym;
 
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = AnonymApplication.class)
 public class AnonymApplicationTests {
 
-    @Test
-    public void contextLoads() {
+
+    @Before
+    public void before() {
+        System.out.println("=============测试开始=============");
     }
+
+    @After
+    public void after() {
+        System.out.println("=============测试结束=============");
+    }
+
 
 }
